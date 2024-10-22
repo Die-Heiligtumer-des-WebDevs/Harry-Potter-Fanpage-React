49d84fa86306e2baa5beea3b7d3996bd85d0fe7e
Yasin Erden <yasin.erden@dci-student.org>
2024-10-22 15:53:34 +0200

    Harry-Potter-Logo-Component

---
+++ src/components/SocialMedialIcons.jsx
@@ -0,0 +1,16 @@
+import { FaInstagram, FaYoutube, FaTwitter, FaFacebook } from "react-icons/fa";
+
+import "../styles/components/socialMedialIcons.scss"
+
+const SocialMedia = () => {
+    return(
+        <div className="socialMedia-box">
+            <span><FaInstagram className="socialMedia-icons"/></span>
+            <span><FaYoutube  className="socialMedia-icons"/></span>
+            <span><FaTwitter  className="socialMedia-icons"/></span>
+            <span><FaFacebook className="socialMedia-icons"/></span>
+        </div>
+    )
+}
+
+export default SocialMedia;
