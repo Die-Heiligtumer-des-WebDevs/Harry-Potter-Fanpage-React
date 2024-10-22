b91c61ea4c14447dcadc1d93be154b2adfd350de
Yasin Erden <yasin.erden@dci-student.org>
2024-10-22 13:43:20 +0200

    Icons-jsx+style.scss angelegt und geschrieben

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { AppRouter } from "./utils/AppRouter.jsx";
 import { AppProvider } from "./utils/AppContextCharacters.jsx";
+import SocialMedia from "./components/Icons.jsx";
 // import "./style/index.scss";
 
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AppProvider>
       <RouterProvider router={AppRouter} />
+      <SocialMedia />
     </AppProvider>
   </StrictMode>
 );
