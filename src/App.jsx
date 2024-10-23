55560a74b9ed470c5666dc6920df23b2c9486753
Yasin Erden <yasin.erden@dci-student.org>
2024-10-23 12:53:44 +0200

    MenuButton fertig und AppContext verbessert

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-     {/* <Header /> */}
+         {/* <Header /> */}
       <main>
         <Outlet />
       </main>
--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import  AppRouter  from "./utils/AppRouter.jsx";
-import  AppProviderCharacters  from "./utils/AppContextCharacters.jsx";
-import AppProviderSpells from "./utils/AppContextSpells.jsx";
+import AppProvider from "./utils/AppContext.jsx";
 
+import Button from "./components/MenuButtons.jsx"
 // import "./style/index.scss";
 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppProviderCharacters>
-      <AppProviderSpells>
+      <AppProvider>
         <RouterProvider router={AppRouter} />
-      </AppProviderSpells>
-    </AppProviderCharacters>
+      </AppProvider>
   </StrictMode>
 );
