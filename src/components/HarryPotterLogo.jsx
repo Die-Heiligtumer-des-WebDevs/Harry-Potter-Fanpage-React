b9377da7c769f1eeb9487ffa8a8372fc1aea875d
Yasin Erden <yasin.erden@dci-student.org>
2024-10-28 21:55:02 +0100

    styling anpassungen und css in SCSS umgewandelt

--- src/components/HarryPotterLogo.jsx
+++ src/components/HarryPotterLogo.jsx
@@ -1,12 +1,13 @@
 import "../styles/components/harryPotterLogo.scss"
 import logo from "../assets/harryPotterLogo/logo-gold.png";
+import {Link} from "react-router-dom"
 
 const HPLogo = () => {
     return (
         // den href pfad richtig angeben um auf die richtige seite zu kommen
-        <a href="/">
+        <Link href="/">
             <img className="HPLogo" src={logo} alt="Harry Potter Logo" />
-        </a>
+        </Link>
     );
 };
 
